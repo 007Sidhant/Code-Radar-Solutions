cd0ef1f0f048902e7dfa22ac06d6a5fe8c5575df
Sidhant <sidhant007.28@gmail.com>
2025-02-13 22:49:52 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    char word;
+    scanf("%c", &word);
+    switch(word){
+        case A:
+            printf("Excellent \n");
+            break;
+        case B:
+            printf("Good \n");
+            break;
+        case C:
+            printf("Average \n");
+            break;
+        case D:
+            printf("Below Average \n");
+            break;
+        case F:
+            printf("Fail \n");
+            break;
+        default:
+            printf("Invalid grade");
+
+    }
+    return 0;
+}
